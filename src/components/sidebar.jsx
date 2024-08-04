c3abef209307620b1370ffc065816bf9a4759026
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-08-04 23:14:07 +0100

    Edited the dashboard

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Outlet, Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft,  } from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -8,7 +10,7 @@ const Sidebar = () => {
        <Link to={'/Profile'} ><button className={'sidebar-link'}>Profile</button></Link> 
        <Link to={'/Settings'} ><button className={'sidebar-link'}>Settings</button></Link> 
     </div>
-       <Link to={'/'} ><button className={'back-home'}>Go back</button></Link> 
+       <Link to={'/'} ><button className={'back-home'}><FontAwesomeIcon icon={faArrowLeft}/> Back Home</button></Link> 
     </div>
   )
 }
